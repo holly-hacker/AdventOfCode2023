273e74b851d64e12b64bc2e23a144e6fcfb2ed50
HoLLy <dario.db@hotmail.com>
2023-12-20 11:05:41 +0100

    Clean up day 20 solution

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -12,24 +12,16 @@ impl SolutionSilver<usize> for Day {
     const INPUT_REAL: &'static str = include_str!("input_real.txt");
 
     fn calculate_silver(input: &str) -> usize {
-        // %: flip-plop. flips if receiving low pulse. when flipped, send high or low depending on
-        // new state. initial state is low so first sent state is high.
-        // &: conjunction. remembers last state for all inputs. if all states are high, send high.
-        // otherwise send low.
-        // broadcast: passes along input to outputs.
-        // start of loop is sending `low` to broadcast.
-
         let map = input
             .lines()
             .map(|l| {
-                let (start, end) = l.split_once(" -> ").unwrap();
+                let (start, targets) = l.split_once(" -> ").unwrap();
 
                 let (prefix, module) = match start.chars().next().unwrap() {
                     '&' => (Some('&'), start.trim_start_matches('&')),
                     '%' => (Some('%'), start.trim_start_matches('%')),
                     _ => (None, start),
                 };
-                let targets = end.split(", ").collect::<Vec<_>>();
 
                 (module, (prefix, targets))
             })
@@ -39,103 +31,65 @@ impl SolutionSilver<usize> for Day {
         let mut nodes = map
             .iter()
             .map(|(module, (prefix, targets))| {
+                let targets = targets.split(", ").collect::<Vec<_>>();
                 let node = match prefix {
-                    Some('%') => Node::FlipFlop {
-                        state: false,
-                        targets: targets.clone(),
-                    },
+                    Some('%') => Node::FlipFlop { state: false },
                     Some('&') => Node::Conjunction {
                         input_states: map
                             .iter()
                             .filter(|(_, (_, v_targets))| v_targets.contains(module))
                             .map(|(k, (_, _))| (*k, false))
                             .collect::<Vec<_>>(),
-                        targets: targets.clone(),
                     },
-                    None => Node::Broadcast {
-                        targets: targets.clone(),
-                    },
+                    None => Node::Broadcast,
                     _ => unreachable!(),
                 };
 
-                (*module, node)
+                (*module, (node, targets))
             })
             .collect::<AHashMap<_, _>>();
 
-        let mut count_low = 0;
         let mut count_high = 0;
+        let mut count_low = 0;
 
         let mut queue = VecDeque::new();
         for _iteration in 0..1000 {
-            let should_print = false; // iteration == 0;
-
             queue.push_back(("button", "broadcaster", false));
-            should_print.then(|| println!("button -low-> broadcaster"));
 
-            while let Some((from, to, signal)) = queue.pop_front() {
-                if signal {
+            while let Some((from, to, incoming_signal)) = queue.pop_front() {
+                if incoming_signal {
                     count_high += 1;
                 } else {
                     count_low += 1;
                 }
 
-                let Some(node) = nodes.get_mut(to) else {
+                let Some((node, targets)) = nodes.get_mut(to) else {
                     continue;
                 };
-                match node {
-                    Node::FlipFlop { state, targets } => {
-                        if !signal {
-                            // flip if low
-                            *state = !*state;
 
-                            for target in targets {
-                                should_print.then(|| {
-                                    println!(
-                                        "{to} -{}-> {}",
-                                        if *state { "high" } else { "low" },
-                                        target
-                                    );
-                                });
-                                queue.push_back((to, *target, *state));
-                            }
+                let outgoing_signal = match node {
+                    Node::FlipFlop { state } => {
+                        if incoming_signal {
+                            continue;
                         }
-                    }
-                    Node::Conjunction {
-                        input_states,
-                        targets,
-                    } => {
-                        input_states.iter_mut().for_each(|(k, v)| {
-                            if *k == from {
-                                *v = signal
-                            }
-                        });
-                        // input_states.insert(from, signal);
 
-                        let to_send = !input_states.iter().all(|(_, v)| *v);
+                        // flip if low
+                        *state = !*state;
 
-                        for target in targets {
-                            should_print.then(|| {
-                                println!(
-                                    "{to} -{}-> {}",
-                                    if to_send { "high" } else { "low" },
-                                    target
-                                );
-                            });
-                            queue.push_back((to, *target, to_send));
-                        }
+                        *state
                     }
-                    Node::Broadcast { targets } => {
-                        for target in targets {
-                            should_print.then(|| {
-                                println!(
-                                    "{to} -{}-> {}",
-                                    if signal { "high" } else { "low" },
-                                    target
-                                );
-                            });
-                            queue.push_back((to, *target, signal));
+                    Node::Conjunction { input_states } => {
+                        if let Some((_, v)) = input_states.iter_mut().find(|(k, _)| *k == from) {
+                            *v = incoming_signal;
                         }
+
+                        !input_states.iter().all(|(_, v)| *v)
                     }
+                    Node::Broadcast => incoming_signal,
+                };
+
+                for target in targets {
+                    queue.push_back((to, *target, outgoing_signal));
                 }
             }
         }
@@ -163,8 +117,7 @@ impl SolutionGold<usize, usize> for Day {
             .collect::<AHashMap<_, _>>();
 
         if !map.values().any(|v| v.1.contains(&"rx")) {
-            // println!("no rx");
-            return 0;
+            return usize::MAX;
         }
 
         // convert parsed lines to nodes
@@ -172,127 +125,83 @@ impl SolutionGold<usize, usize> for Day {
             .iter()
             .map(|(module, (prefix, targets))| {
                 let node = match prefix {
-                    Some('%') => Node::FlipFlop {
-                        state: false,
-                        targets: targets.clone(),
-                    },
+                    Some('%') => Node::FlipFlop { state: false },
                     Some('&') => Node::Conjunction {
                         input_states: map
                             .iter()
                             .filter(|(_, (_, v_targets))| v_targets.contains(module))
                             .map(|(k, (_, _))| (*k, false))
                             .collect::<Vec<_>>(),
-                        targets: targets.clone(),
                     },
-                    None => Node::Broadcast {
-                        targets: targets.clone(),
-                    },
+                    None => Node::Broadcast,
                     _ => unreachable!(),
                 };
 
-                (*module, node)
+                (*module, (node, targets))
             })
             .collect::<AHashMap<_, _>>();
 
+        // run simulation for a while
+        const SAMPLE_SIZE: usize = 10_000;
         let mut queue = VecDeque::new();
         let mut states = vec![];
-        const SAMPLE_SIZE: usize = 100_000;
-        // assume we get the loops in 1m iterations
         for iteration in 0..SAMPLE_SIZE {
-            let should_print = false;
-
             queue.push_back(("button", "broadcaster", false));
-            should_print.then(|| println!("button -low-> broadcaster"));
 
-            while let Some((from, to, signal)) = queue.pop_front() {
-                if to == "rx" && !signal {
+            while let Some((from, to, incoming_signal)) = queue.pop_front() {
+                // this is not going to get hit, but it's here for correctness
+                if to == "rx" && !incoming_signal {
                     return iteration + 1;
                 }
 
-                let Some(node) = nodes.get_mut(to) else {
+                let Some((node, targets)) = nodes.get_mut(to) else {
                     continue;
                 };
-                match node {
-                    Node::FlipFlop { state, targets } => {
-                        if !signal {
-                            // flip if low
-                            *state = !*state;
 
-                            for target in targets {
-                                should_print.then(|| {
-                                    println!(
-                                        "{to} -{}-> {target}",
-                                        if *state { "high" } else { "low" },
-                                    );
-                                });
-                                queue.push_back((to, *target, *state));
-                            }
+                let outgoing_signal = match node {
+                    Node::FlipFlop { state } => {
+                        if incoming_signal {
+                            continue;
                         }
-                    }
-                    Node::Conjunction {
-                        input_states,
-                        targets,
-                    } => {
-                        input_states.iter_mut().for_each(|(k, v)| {
-                            if *k == from {
-                                *v = signal
-                            }
-                        });
-                        // input_states.insert(from, signal);
 
-                        let to_send = !input_states.iter().all(|(_, v)| *v);
+                        // flip if low
+                        *state = !*state;
 
-                        for target in targets {
-                            should_print.then(|| {
-                                println!(
-                                    "{to} -{}-> {target}",
-                                    if to_send { "high" } else { "low" },
-                                );
-                            });
-                            queue.push_back((to, *target, to_send));
-                        }
+                        *state
                     }
-                    Node::Broadcast { targets } => {
-                        for target in targets {
-                            should_print.then(|| {
-                                println!(
-                                    "{to} -{}-> {target}",
-                                    if signal { "high" } else { "low" },
-                                );
-                            });
-                            queue.push_back((to, *target, signal));
+                    Node::Conjunction { input_states } => {
+                        if let Some((_, v)) = input_states.iter_mut().find(|(k, _)| *k == from) {
+                            *v = incoming_signal;
                         }
+
+                        !input_states.iter().all(|(_, v)| *v)
                     }
+                    Node::Broadcast => incoming_signal,
+                };
+
+                for target in *targets {
+                    queue.push_back((to, *target, outgoing_signal));
                 }
             }
 
             // NOTE: this order is unstable, uses compile-time rng!
             let nodes_values = nodes
-                .iter()
-                .map(|(_, v)| (v.get_state()))
+                .values()
+                .map(|v| (v.0.fold_state_to_int()))
                 .collect::<Vec<_>>();
 
-            // println!("{iteration:06}: {nodes_values:?}");
             states.push(nodes_values);
-
-            // for (idx, i) in nodes_values.iter().enumerate() {
-            //     let prev = previous_states.insert((idx, *i), iteration);
-            //     if let Some(prev) = prev {
-            //         println!("loop detected: {} -> {}", prev, iteration);
-            //     }
-            // }
         }
 
-        // we've exited the loop and collected our samples
-        let len = states.first().unwrap().len();
+        // we've run the loop for a while, but we didn't write `high` to `rx`
+        // assume we're in a very large loop and we'll write to `rx` at the end of the loop
+        // this is a stupid assumption, but it seems to be how the challenge was designed
 
-        // println!("looking for loops!");
+        let state_len = states.first().unwrap().len();
         let mut loops = vec![];
-        'len: for i in 0..len {
-            // println!("checking idx {i}");
-
+        'len: for i in 0..state_len {
             // check if this is a loop
-            for loop_len in 1..(SAMPLE_SIZE / 3) {
+            for loop_len in 1.. {
                 let mut state_chunks = states.chunks(loop_len);
                 let first_chunk_of_states = state_chunks.next().unwrap();
                 let is_loop = state_chunks.all(|chunk_of_states| {
@@ -303,7 +212,6 @@ impl SolutionGold<usize, usize> for Day {
                 });
 
                 if is_loop {
-                    // println!("loop found: {loop_len}");
                     loops.push(loop_len);
                     continue 'len;
                 }
@@ -339,38 +247,34 @@ fn gcd(first: usize, second: usize) -> usize {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Node<'i> {
     FlipFlop {
         state: bool,
-        targets: Vec<&'i str>,
     },
     Conjunction {
-        input_states: Vec<(&'i str, bool)>,
-        targets: Vec<&'i str>,
+        input_states: Vec<(&'i str, bool)>, // essentially a map
     },
-    Broadcast {
-        targets: Vec<&'i str>,
-    },
+    Broadcast,
 }
 
 impl Node<'_> {
-    fn get_state(&self) -> u32 {
+    fn fold_state_to_int(&self) -> u32 {
         match self {
-            Node::FlipFlop { state, .. } => {
+            Node::FlipFlop { state } => {
                 if *state {
                     1
                 } else {
                     0
                 }
             }
-            Node::Conjunction { input_states, .. } => input_states
+            Node::Conjunction { input_states } => input_states
                 .iter()
                 .enumerate()
                 .fold(0, |acc, (i, (_, state))| {
                     acc | if *state { 1 << i } else { 0 }
                 }),
-            Node::Broadcast { .. } => 0,
+            Node::Broadcast => 0,
         }
     }
 }
@@ -391,6 +295,12 @@ fn test_silver_sample_2() {
 fn test_silver_real() {
     let output = Day::calculate_silver(Day::INPUT_REAL);
     assert_eq!(712543680, output);
+}
+
+#[test]
+fn test_gold_sample() {
+    let output = Day::calculate_gold(Day::INPUT_SAMPLE);
+    assert_eq!(usize::MAX, output);
 }
 
 #[test]
