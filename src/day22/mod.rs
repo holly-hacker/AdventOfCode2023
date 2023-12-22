77ae9d6c63311d730a87c03c3021d1b91e9d4307
HoLLy <dario.db@hotmail.com>
2023-12-22 18:58:53 +0100

    Somewhat improve complexity of day 22

--- src/day22/mod.rs
+++ src/day22/mod.rs
@@ -1,4 +1,4 @@
-use ahash::AHashSet;
+use ahash::{AHashMap, AHashSet};
 
 use super::*;
 
@@ -10,50 +10,18 @@ impl SolutionSilver<usize> for Day {
     const INPUT_REAL: &'static str = include_str!("input_real.txt");
 
     fn calculate_silver(input: &str) -> usize {
-        let mut bricks = input
-            .lines()
-            .map(|line| {
-                let (start, end) = line.split_once('~').unwrap();
-
-                let (start_x, start) = start.split_once(',').unwrap();
-                let (start_y, start_z) = start.split_once(',').unwrap();
-
-                let (end_x, end) = end.split_once(',').unwrap();
-                let (end_y, end_z) = end.split_once(',').unwrap();
-
-                (
-                    (
-                        start_x.parse::<usize>().unwrap(),
-                        start_y.parse::<usize>().unwrap(),
-                        start_z.parse::<usize>().unwrap(),
-                    ),
-                    (
-                        end_x.parse::<usize>().unwrap(),
-                        end_y.parse::<usize>().unwrap(),
-                        end_z.parse::<usize>().unwrap(),
-                    ),
-                )
-            })
-            .collect::<Vec<_>>();
-
+        let mut bricks = parse_input(input);
         simulate_falling(&mut bricks);
 
-        // bricks have settled
-        // dbg!(&bricks);
-
         // loop over bricks to see which ones are supporting others
-        let mut bricks_1 = Vec::new();
+        let mut unsupported_bricks = Vec::new();
+        let lookup = build_position_lut(&bricks);
         for brick_to_remove_i in 0..bricks.len() {
             for falling_brick in &bricks {
                 let is_on_ground = falling_brick.0 .2 == 1 || falling_brick.1 .2 == 1;
 
                 let is_supported = is_on_ground
-                    || (falling_brick.0 .2..=falling_brick.1 .2)
-                        .flat_map(|z| {
-                            (falling_brick.0 .1..=falling_brick.1 .1).flat_map(move |y| {
-                                (falling_brick.0 .0..=falling_brick.1 .0).map(move |x| (x, y, z))
-                            })
-                        })
+                    || get_brick_iterator(*falling_brick)
                         .filter(|(x, y, z)| {
                             let voxel_below_is_own_shape =
                                 (falling_brick.0 .0..=falling_brick.1 .0).contains(x)
@@ -63,83 +31,37 @@ impl SolutionSilver<usize> for Day {
                             !voxel_below_is_own_shape
                         })
                         .any(|(fall_x, fall_y, fall_z)| {
-                            let is_supported = bricks.iter().enumerate().any(|(other_i, other)| {
-                                let other_x = other.0 .0..=other.1 .0;
-                                let other_y = other.0 .1..=other.1 .1;
-                                let other_z = other.0 .2..=other.1 .2;
-
-                                other_i != brick_to_remove_i
-                                    && other_x.contains(&fall_x)
-                                    && other_y.contains(&fall_y)
-                                    && other_z.contains(&(fall_z - 1))
-                            });
-
+                            let fall_idx = lookup.get(&(fall_x, fall_y, fall_z - 1)).copied();
+                            let is_supported =
+                                matches!(fall_idx, Some(idx) if idx != brick_to_remove_i);
                             is_supported
                         });
 
                 if !is_supported {
-                    // println!(
-                    //     "When {} is removed, {} can fall",
-                    //     char::from(b'A' + brick_to_remove_i as u8),
-                    //     char::from(b'A' + falling_brick_i as u8),
-                    // );
-                    bricks_1.push(brick_to_remove_i);
+                    unsupported_bricks.push(brick_to_remove_i);
                     break;
                 }
             }
         }
 
-        bricks.len() - bricks_1.len()
+        bricks.len() - unsupported_bricks.len()
     }
 }
 
 impl SolutionGold<usize, usize> for Day {
     fn calculate_gold(input: &str) -> usize {
-        let mut bricks = input
-            .lines()
-            .map(|line| {
-                let (start, end) = line.split_once('~').unwrap();
-
-                let (start_x, start) = start.split_once(',').unwrap();
-                let (start_y, start_z) = start.split_once(',').unwrap();
-
-                let (end_x, end) = end.split_once(',').unwrap();
-                let (end_y, end_z) = end.split_once(',').unwrap();
-
-                (
-                    (
-                        start_x.parse::<usize>().unwrap(),
-                        start_y.parse::<usize>().unwrap(),
-                        start_z.parse::<usize>().unwrap(),
-                    ),
-                    (
-                        end_x.parse::<usize>().unwrap(),
-                        end_y.parse::<usize>().unwrap(),
-                        end_z.parse::<usize>().unwrap(),
-                    ),
-                )
-            })
-            .collect::<Vec<_>>();
-
+        let mut bricks = parse_input(input);
         simulate_falling(&mut bricks);
 
-        // bricks have settled
-        // dbg!(&bricks);
-
         // loop over bricks to see which ones are supporting others
-        let mut bricks_1 = Vec::new();
+        let mut unsupported_bricks = Vec::new();
+        let lookup = build_position_lut(&bricks);
         for brick_to_remove_i in 0..bricks.len() {
-            // println!("To remove: {brick_to_remove_i}/{}", bricks.len());
             for falling_brick in &bricks {
                 let is_on_ground = falling_brick.0 .2 == 1 || falling_brick.1 .2 == 1;
 
                 let is_supported = is_on_ground
-                    || (falling_brick.0 .2..=falling_brick.1 .2)
-                        .flat_map(|z| {
-                            (falling_brick.0 .1..=falling_brick.1 .1).flat_map(move |y| {
-                                (falling_brick.0 .0..=falling_brick.1 .0).map(move |x| (x, y, z))
-                            })
-                        })
+                    || get_brick_iterator(*falling_brick)
                         .filter(|(x, y, z)| {
                             let voxel_below_is_own_shape =
                                 (falling_brick.0 .0..=falling_brick.1 .0).contains(x)
@@ -149,42 +71,82 @@ impl SolutionGold<usize, usize> for Day {
                             !voxel_below_is_own_shape
                         })
                         .any(|(fall_x, fall_y, fall_z)| {
-                            let is_supported = bricks.iter().enumerate().any(|(other_i, other)| {
-                                let other_x = other.0 .0..=other.1 .0;
-                                let other_y = other.0 .1..=other.1 .1;
-                                let other_z = other.0 .2..=other.1 .2;
-
-                                other_i != brick_to_remove_i
-                                    && other_x.contains(&fall_x)
-                                    && other_y.contains(&fall_y)
-                                    && other_z.contains(&(fall_z - 1))
-                            });
-
+                            let fall_idx = lookup.get(&(fall_x, fall_y, fall_z - 1)).copied();
+                            let is_supported =
+                                matches!(fall_idx, Some(idx) if idx != brick_to_remove_i);
                             is_supported
                         });
 
                 if !is_supported {
                     let mut cloned = bricks.clone();
-                    cloned.remove(brick_to_remove_i);
+                    cloned.swap_remove(brick_to_remove_i);
                     let would_fall = simulate_falling(&mut cloned);
-                    // println!(
-                    //     "When {} is removed, {} can fall",
-                    //     char::from(b'A' + brick_to_remove_i as u8),
-                    //     char::from(b'A' + falling_brick_i as u8),
-                    // );
-                    bricks_1.push(would_fall);
+                    unsupported_bricks.push(would_fall);
                     break;
                 }
             }
         }
 
-        bricks_1.into_iter().sum()
+        unsupported_bricks.into_iter().sum()
     }
 }
 
-fn simulate_falling(bricks: &mut Vec<((usize, usize, usize), (usize, usize, usize))>) -> usize {
+fn parse_input(input: &str) -> Vec<((usize, usize, usize), (usize, usize, usize))> {
+    input
+        .lines()
+        .map(|line| {
+            let (start, end) = line.split_once('~').unwrap();
+
+            let (start_x, start) = start.split_once(',').unwrap();
+            let (start_y, start_z) = start.split_once(',').unwrap();
+
+            let (end_x, end) = end.split_once(',').unwrap();
+            let (end_y, end_z) = end.split_once(',').unwrap();
+
+            (
+                (
+                    start_x.parse::<usize>().unwrap(),
+                    start_y.parse::<usize>().unwrap(),
+                    start_z.parse::<usize>().unwrap(),
+                ),
+                (
+                    end_x.parse::<usize>().unwrap(),
+                    end_y.parse::<usize>().unwrap(),
+                    end_z.parse::<usize>().unwrap(),
+                ),
+            )
+        })
+        .collect::<Vec<_>>()
+}
+
+fn get_brick_iterator(
+    brick: ((usize, usize, usize), (usize, usize, usize)),
+) -> impl Iterator<Item = (usize, usize, usize)> {
+    (brick.0 .2..=brick.1 .2).flat_map(move |z| {
+        (brick.0 .1..=brick.1 .1)
+            .flat_map(move |y| (brick.0 .0..=brick.1 .0).map(move |x| (x, y, z)))
+    })
+}
+
+fn build_position_lut(
+    bricks: &[((usize, usize, usize), (usize, usize, usize))],
+) -> AHashMap<(usize, usize, usize), usize> {
+    let mut map = AHashMap::new();
+
+    for (i, brick) in bricks.iter().enumerate() {
+        get_brick_iterator(*brick).for_each(|(x, y, z)| {
+            map.insert((x, y, z), i);
+        });
+    }
+
+    map
+}
+
+fn simulate_falling(bricks: &mut [((usize, usize, usize), (usize, usize, usize))]) -> usize {
     let mut changed = true;
     let mut fallen = AHashSet::new();
+
+    let mut lookup = build_position_lut(bricks);
     while changed {
         changed = false;
 
@@ -196,32 +158,35 @@ fn simulate_falling(bricks: &mut Vec<((usize, usize, usize), (usize, usize, usiz
                 continue;
             }
 
-            let can_fall = (own.0 .2..=own.1 .2)
-                .flat_map(|z| {
-                    (own.0 .1..=own.1 .1)
-                        .flat_map(move |y| (own.0 .0..=own.1 .0).map(move |x| (x, y, z)))
+            let can_fall = get_brick_iterator(own)
+                .filter(|(x, y, z)| {
+                    let voxel_below_is_own_shape = (own.0 .0..=own.1 .0).contains(x)
+                        && (own.0 .1..=own.1 .1).contains(y)
+                        && (own.0 .2..=own.1 .2).contains(&(z - 1));
+
+                    !voxel_below_is_own_shape
                 })
                 .all(|(x, y, z)| {
-                    ((own.0 .0..=own.1 .0).contains(&x)
-                        && (own.0 .1..=own.1 .1).contains(&y)
-                        && (own.0 .2..=own.1 .2).contains(&(z - 1)))
-                        || !bricks.iter().any(|other| {
-                            let other_x = other.0 .0..=other.1 .0;
-                            let other_y = other.0 .1..=other.1 .1;
-                            let other_z = other.0 .2..=other.1 .2;
-
-                            other_x.contains(&x)
-                                && other_y.contains(&y)
-                                && other_z.contains(&(z - 1))
-                        })
+                    let shape_idx = lookup.get(&(x, y, z - 1)).copied();
+                    let is_supported = matches!(shape_idx, Some(idx) if idx != own_i);
+                    !is_supported
                 });
 
             if can_fall {
-                // println!("Falling brick {:?}", own_i);
                 bricks[own_i].0 .2 -= 1;
                 bricks[own_i].1 .2 -= 1;
                 changed = true;
                 fallen.insert(own_i);
+
+                // patch the lookup table instead of fully rebuilding it
+                lookup.retain(|_, v| *v != own_i);
+                for z in own.0 .2..=own.1 .2 {
+                    for y in own.0 .1..=own.1 .1 {
+                        for x in own.0 .0..=own.1 .0 {
+                            lookup.insert((x, y, z - 1), own_i);
+                        }
+                    }
+                }
             }
         }
     }
